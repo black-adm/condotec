2b76505be0d62fb3cbbe833149ee6019e794bd65
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2024-01-17 16:05:08 +0100

    FEAT: Starting new implementations on the project

--- backend/Condotec.Management/CondoTec.Management.API/Program.cs
+++ backend/Condotec.Management/CondoTec.Management.API/Program.cs
@@ -1,20 +1,47 @@
+using CondoTec.Management.API.Extensions;
+using CondoTec.Management.IoC.Conf;
+using CondoTec.Management.IoC.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var enviroment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-builder.Services.AddControllers();
+builder.Configuration
+    .AddJsonFile("appsettings.json", true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{enviroment}.json", true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+var applicationSettings = builder.Configuration.GetSection("Settings").Get<Settings>();
+
+builder.Services.AddSingleton<ISettings>(applicationSettings!);
+
+builder.Logging
+    .ClearProviders()
+    .AddFilter("Microsoft", LogLevel.Warning)
+    .AddFilter("Microsoft", LogLevel.Critical);
+
+// Add services to the container.
+builder.Services
+    .AddAuth(applicationSettings?.TokenAuth)
+    .AddLoggingDependency()
+    .AddControllers(options => options.Filters.Add(typeof(ValidationAttribute)))
+    .AddNewtonsoftJson()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+        options.SuppressInferBindingSourcesForParameters = true;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwagger();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Condotec.Management.API"));
 
 app.UseHttpsRedirection();
 
---
+++ backend/Condotec.Management/CondoTec.Management.IoC/Extensions/ValidatorsExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CondoTec.Management.IoC.Extensions
+{
+    public class ValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values.Where(v => v.Errors.Count > 0)
+                        .SelectMany(v => v.Errors)
+                        .Select(v => v.ErrorMessage)
+                        .ToList();
+
+                context.Result = new JsonResult(new
+                {
+                    Code = 400,
+                    Message = "One or more validation errors occurred.",
+                    Errors = errors
+                })
+                {
+                    StatusCode = 200
+                };
+            }
+        }
+    }
+}
