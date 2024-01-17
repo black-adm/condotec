d5594676ee3f17d3a1ad8dd9e5342218c06e011e
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2024-01-17 18:10:13 +0100

    FIX: Reorganing project

---
+++ backend/Condotec.Management/src/CondoTec.Management.API/Controllers/CondominioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CondoTec.Management.API.Controllers
+{
+    public class CondominioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
